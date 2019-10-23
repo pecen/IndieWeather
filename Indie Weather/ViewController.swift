5d665048d7465debe1851dc61e76e0b4476aac2a
Peter Centellini <peter.centellini@redesajn.com>
2019-10-23 20:30:38 +0200

    Initial Commit

---
+++ Indie Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Indie Weather
+//
+//  Created by Peter Centellini on 2019-10-23.
+//  Copyright © 2019 Redesajn Interactive Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
